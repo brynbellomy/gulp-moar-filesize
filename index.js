51225144064c505c173a00a2f204a3b5c6df73ba
bryn austin bellomy <bryn.bellomy@gmail.com>
2015-06-21 20:01:21 -0500

    Updated to work with stream.Readables

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 var map = require('map-stream');
 var gutil = require('gulp-util');
 var getFileSize = require("filesize");
+var stream = require('stream');
 
-module.exports = function(){
-  'use strict';
+module.exports = function() {
+    'use strict';
 
+    return map(function(file, callback) {
+        var filenameShort = file.path.split(/\/|\\/).pop()
 
-  return map(function(file,callback){
-  	var filenameShort = file.path.split(/\/|\\/).pop()
-
-  	//Check if file.stat exists (gulp.concat removes it for example)
-  	var filesize = file.stat ? getFileSize(file.stat.size) : getFileSize(Buffer.byteLength(String(file.contents)));
-
-  	gutil.log("Size",gutil.colors.cyan(filenameShort),":",gutil.colors.magenta(filesize));
-
-  	callback(null,file)
-  });
-
-
+        //Check if file.stat exists (gulp.concat removes it for example)
+        if (file.contents instanceof stream.Readable) {
+            var totalSize = 0;
+            file.contents
+                .on('data', function (chunk) {
+                    totalSize += chunk.length;
+                })
+                .on('end', function() {
+                    printSizeAndReturn(totalSize, file, filenameShort);
+                });
+        }
+        else {
+            var totalSize = file.stat ? file.stat.size : Buffer.byteLength(file.contents.toString('utf8'));
+            printSizeAndReturn(totalSize, file, filenameShort);
+        }
+    });
 };
+
+function printSizeAndReturn (filesize, file, filenameShort) {
+    filesize = getFileSize(filesize)
+    gutil.log('Size', gutil.colors.cyan(filenameShort), ":", gutil.colors.magenta(filesize));
+    callback(null, file);
+}
